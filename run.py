0bf3333a29c944950fb23f5a00806c97d552d559
aleksl0l <aleksl0l@yandex.ru>
2018-05-16 19:15:28 +0300

    add run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+from app.app import app
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    else:
+        host = '127.0.0.1'
+    app.run(host=host)
